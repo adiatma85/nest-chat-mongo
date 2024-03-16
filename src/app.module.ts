997f8c77c77ec0ac1496f5b9271e2bef9dd18922
Koernia <adiatma85@gmail.com>
2024-03-16 21:03:02 +0700

    make new schema prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { ChatModule } from './chat/chat.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PtestingModule } from './ptesting/ptesting.module';
-
 
 // Environment variables
 let jwtSecret : string
 let mongoDBUri : string
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(
-      {
-        isGlobal: true,
-      }
-    ),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: mongoDBUri,
-      }),
-      inject: [ConfigService],
-
-    }),
-    JwtModule.register({
-      global: true,
-      secret: jwtSecret || "secret",
-      signOptions: { expiresIn: '7d' },
-    }),
-    AuthModule,
-    UserModule,
-    ChatModule,
-    PtestingModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AddContextAttribute } from './middleware/context.interceptor';
-import { HttpExceptionFilter } from './response/response.failed.filter';
-import { ResponseInterceptor } from './response/response.success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-
 
   // Enabling cors
   app.enableCors();
@@ -18,13 +14,13 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // Enable global filter for http exception filter
-  app.useGlobalFilters(new HttpExceptionFilter())
+  // app.useGlobalFilters(new HttpExceptionFilter())
 
   // Enabling context interceptor
-  app.useGlobalInterceptors(
-    new AddContextAttribute(),
-    new ResponseInterceptor()
-  );
+  // app.useGlobalInterceptors(
+  //   new AddContextAttribute(),
+  //   new ResponseInterceptor()
+  // );
 
   // Swagger setup
   const config = new DocumentBuilder()
@@ -32,10 +28,6 @@ async function bootstrap() {
     .setDescription('The API description')
     .setVersion('1.0')
     .addBearerAuth()
-    .addTag('auth')
-    .addTag('chat')
-    .addTag('users')
-    .addTag('ptesting')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/doc', app, document);
