394764d3d6979dac41a7f9ee256452941aabac9d
Koernia <adiatma85@gmail.com>
2024-03-20 00:14:30 +0700

    create token service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,24 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { TokenService } from './token/token.service';
 
+
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      // retry strategy
+      return Math.min(times * 50, 2000);
+    },
+  },
+});
+
+
+// Ngelanjut di sini nanti
 @Module({
   imports: [
     AuthModule,
@@ -23,29 +40,29 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (
         configService: ConfigService,
 
-        // tokenService: TokenService,
+        tokenService: TokenService,
       ) => {
         return {
           playground: true,
           autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
           sortSchema: true,
-          // installSubscriptionHandlers: true,
-          // subscriptions: {
-          //   'graphql-ws': true,
-          //   'subscriptions-transport-ws': true,
-          // },
-          // onConnect: (connectionParams) => {
-          //   const token = tokenService.extractToken(connectionParams);
+          installSubscriptionHandlers: true,
+          subscriptions: {
+            'graphql-ws': true,
+            'subscriptions-transport-ws': true,
+          },
+          onConnect: (connectionParams) => {
+            const token = tokenService.extractToken(connectionParams);
 
-          //   if (!token) {
-          //     throw new Error('Token not provided');
-          //   }
-          //   const user = tokenService.validateToken(token);
-          //   if (!user) {
-          //     throw new Error('Invalid token');
-          //   }
-          //   return { user };
-          // },
+            if (!token) {
+              throw new Error('Token not provided');
+            }
+            const user = tokenService.validateToken(token);
+            if (!user) {
+              throw new Error('Invalid token');
+            }
+            return { user };
+          },
           // context: ({ req, res, connection }) => {
           //   if (connection) {
           //     return { req, res, user: connection.context.user, pubSub }; // Injecting pubSub into context
@@ -60,6 +77,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TokenService],
 })
-export class AppModule {}
+export class AppModule { }
