17d0f79676dc46a8592110a8d9b4cdabdbec7c34
ramdani koernia <adiatma85@gmail.com>
2024-05-13 22:58:11 +0700

    modify resolver, schema, and user service for gql

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -62,4 +62,27 @@ export class UserService {
             },
         });
     }
+
+    async getUsersOfChatroom(chatroomId: number) {
+        return this.prisma.user.findMany({
+            where: {
+                chatrooms: {
+                    some: {
+                        id: chatroomId,
+                    },
+                },
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+    }
+
+    async getUser(userId: number) {
+        return this.prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+    }
 }
